78bec173f1e2f0c3a4dc315cc90c0ff2c59f0597
Nuttasit <nuttasit.kaewthong@gmail.com>
2021-04-27 02:18:09 +0700

    second commit

--- index.js
+++ index.js
@@ -132,6 +132,6 @@ app.post('/add', (req, res) => {
 })
 
 
-app.listen('3001', () => {
-    console.log('Server is running on port 3001')
+app.listen(port, () => {
+    console.log(`Server is running on port . ${port}`)
 })
