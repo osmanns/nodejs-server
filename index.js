09e6ff6ceeca6c59f4b99c876d5bd6fca49885c0
Nuttasit <nuttasit.kaewthong@gmail.com>
2021-04-27 17:30:25 +0700

    twenty fif commit

--- index.js
+++ index.js
@@ -20,8 +20,20 @@ var port = process.env.PORT || 8080
 // }
 // app.use(cors(corsOptions));
 
+app.use(cors(), function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://foodchoiceserver.herokuapp.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
+
+app.use(function(req, res, next) {  
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});  
 
 const db = mysql.createConnection({
     host: "foodch.kaseamsanth.tk",
