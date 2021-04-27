c2aba3337f2a38d3eb0f49a0e7a73d29d85f284e
Nuttasit <nuttasit.kaewthong@gmail.com>
2021-04-27 17:21:43 +0700

    twenty fr commit

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ var port = process.env.PORT || 8080
 //     next();
 // });
 
-const corsOptions ={
-    origin: 'https://foodchoiceserver.herokuapp.com', 
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin: 'https://foodchoiceserver.herokuapp.com', 
+//     credentials: true,            //access-control-allow-credentials:true
+//     optionSuccessStatus: 200
+// }
+// app.use(cors(corsOptions));
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
