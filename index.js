8611f864e5f3d81797194bada7a4734be0e8351c
Nuttasit <nuttasit.kaewthong@gmail.com>
2021-04-27 09:44:05 +0700

    f commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const db = mysql.createConnection({
     // database: "fooddb",
 })
 
-app.get('/', function(req, res, next) {
+app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
