68299910927ee95e818ac03347970baf9e135924
Nuttasit <nuttasit.kaewthong@gmail.com>
2021-04-30 12:24:11 +0700

    oneone commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var host = process.env.HOST || '0.0.0.0';
 var port = process.env.PORT || 8080
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://foodchoicenodejs.herokuapp.com"); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Credentials', true); 
@@ -21,7 +21,7 @@ const db = mysql.createConnection({
     user: "plan",
     password: "Koo112234#",
     database: "fooddb",
-    port: 3306,
+    // port: 3306,
 })
 
 app.get('/', (req, res) => {
