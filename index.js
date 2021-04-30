66d9ad6b265c39c53f692a2f5bde2727180375f5
Nuttasit <nuttasit.kaewthong@gmail.com>
2021-04-30 12:19:38 +0700

    fif fif commit

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const cors = require('cors');
 var host = process.env.HOST || '0.0.0.0';
 var port = process.env.PORT || 8080
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.header('Access-Control-Allow-Credentials', true); 
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Credentials', true); 
+  next();
+});
 
 app.use(cors());
 app.use(express.json());
