e0fbbec5cc5922476f33d75c748ef7f3586b403b
Nuttasit <nuttasit.kaewthong@gmail.com>
2021-04-30 12:44:18 +0700

    tretre commit

--- index.js
+++ index.js
@@ -13,8 +13,15 @@ var port = process.env.PORT || 8080
 //     res.header('Access-Control-Allow-Credentials', true); 
 // });
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    next();
+});
 
 const db = mysql.createConnection({
     host: "foodch.kaseamsanth.tk",
